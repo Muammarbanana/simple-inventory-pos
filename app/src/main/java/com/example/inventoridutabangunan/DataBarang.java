03bb8de84896dddf7d768dc2886bcfa2415271a7
Muammarbanana <muammarfanani2@gmail.com>
2019-05-20 05:51:26 +0700

    Designing item of recycleview data barang

--- app/src/main/java/com/example/inventoridutabangunan/DataBarang.java
+++ app/src/main/java/com/example/inventoridutabangunan/DataBarang.java
@@ -13,7 +13,7 @@ public class DataBarang extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState){
+                             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_data_barang, container, false);
 
         return rootView;
